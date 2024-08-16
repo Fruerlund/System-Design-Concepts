999ab36cf9e9943d5de083665f41a8b7207e0490
root <root@LAPTOP-M65IV45R.localdomain>
2024-08-16 12:31:40 +0200

    Added a simple flask server to test api

--- src/server.py
+++ src/server.py
@@ -1,13 +1,72 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+#curl http://127.0.0.1:33333/api --data "cmd=set&key=abc&value=def"
+#curl http://127.0.0.1:33333/api --data "cmd=get&key=abc"
+
+class API(object):
+    
+    def __init__(self, ip, port):
+        print(self)
+        self.ip = ip
+        self.port = port
+        
+        self.url = "http://{}:{}".format(self.ip, str(self.port))
+        
+    def add(self, key, value):
+        
+        return requests.post(self.url, data={
+            "cmd":"SET",
+            key:value
+        })
+        
+
+    def delete(self, key):
+        return requests.post(self.url, data={
+            "cmd":"REM",
+            "key":key
+        })
+
+    def lookup(self, key):
+        return requests.post(self.url, data={
+            "cmd":"GET",
+            "key":key
+        })
 
 
+app = Flask(__name__)
+api = API("127.0.0.1", 5556)
 
-def main():
-	
-	print("Load")
+@app.route("/")
+def root():
+    return ""
 
-	pass
+@app.route("/api", methods=("GET", "POST"))
+def insert():
+    
+    if request.method == 'POST':
+     
+        if "cmd" in request.form.keys():
+            
+            command = request.form["cmd"].upper()
+
+            if(command == "GET"):
+                r = api.lookup( request.form["key"] )
+                return r.text
+            if(command == "SET"):
+                key = request.form["key"]
+                value = request.form["value"]
+                r = api.add(key, value)
+                return r.text
+            if(command == "REM"):
+                r = api.delete(request.form["key"])
+                return r.text
+          
+            return "OK\r\n"
+    
+    
+    return "\r\n"
 
 
-if __name__ == "__main__":
-
-	main()
+app.run(port=33333, debug=True)
+    
